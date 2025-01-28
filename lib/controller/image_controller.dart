8b074067899d3b146ebfa0f4f2a73907f1b4dcd0
João Antônio <joaoantoniolaraujo@gmail.com>
2025-01-28 20:41:03 -0300

    arrumando alteracoes

--- lib/controller/image_controller.dart
+++ lib/controller/image_controller.dart
@@ -127,7 +127,7 @@ class ImagePickerUtil {
                         return;
                       }
                       setImage!(foto);
-                              setPdf!(null);
+                      setPdf!(null);
 
                       Navigator.pop(context);
                     },
@@ -151,8 +151,12 @@ class ImagePickerUtil {
                         final imageController = ImageController();
                         final pdf = await imageController.pegarArquivo(context);
                     
-                        setPdf!(pdf!);
-                        setImage!(null);
+                        if (pdf != null) {
+                          setPdf!(pdf);
+                          setImage!(null);
+                        } else {
+                          snackAtencao(context, "Nenhum documento selecionado");
+                        }
                     
                         Navigator.pop(context);
                       },
@@ -176,71 +180,6 @@ class ImagePickerUtil {
           ),
         );
       },
-    );
-  }
-}
-
-class TakePictureScreen extends StatefulWidget {
-  final CameraDescription camera;
-  final Function(Uint8List) onPictureTaken;
-
-  const TakePictureScreen({
-    super.key,
-    required this.camera,
-    required this.onPictureTaken,
-  });
-
-  @override
-  _TakePictureScreenState createState() => _TakePictureScreenState();
-}
-
-class _TakePictureScreenState extends State<TakePictureScreen> {
-  late CameraController _controller;
-  late Future<void> _initializeControllerFuture;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = CameraController(
-      widget.camera,
-      ResolutionPreset.high,
-    );
-    _initializeControllerFuture = _controller.initialize();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Tirar foto')),
-      body: FutureBuilder<void>(
-        future: _initializeControllerFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return CameraPreview(_controller);
-          } else {
-            return const Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          try {
-            await _initializeControllerFuture;
-            final image = await _controller.takePicture();
-            final bytes = await image.readAsBytes();
-            widget.onPictureTaken(bytes);
-          } catch (e) {
-            print(e);
-          }
-        },
-        child: const Icon(Icons.camera_alt),
-      ),
     );
   }
 }
